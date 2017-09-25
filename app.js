653c3024ebf717805903c655142bdc6ec2df6392
Kat Cosgrove <kat.cosgrove@gmail.com>
2017-09-25 15:11:05 -0700

    second store fully functional

--- app.js
+++ app.js
@@ -47,27 +47,61 @@ console.log('Average Cookies per Hour ' + first.avgCookiesHour);
 console.log(first.cookiesArray);
 console.log(first.totalCookies);
 
+var seatac = {
+  storeName: 'SeaTac Airport',
+  minCustomers: 3,
+  maxCustomers: 24,
+  avgSale: 1.2,
+  avgCustomersHour: 0,
+  avgCookiesHour: 0,
+  cookiesArray:[],
+  totalCookies: 0,
+  getAvgCustomersHour: function (){
+    this.avgCustomersHour = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1) + this.minCustomers);
+  },
+  getAvgCookiesHour: function () {
+    this.avgCookiesHour = Math.ceil(this.avgCustomersHour * this.avgSale);
+  },
+  render: function () {
+    for (var i = 0; i < days.length; i++) {
+      this.getAvgCustomersHour();
+      this.getAvgCookiesHour();
+      var liEl = document.createElement('li');
+      liEl.textContent = days[i] + ' ' + this.avgCookiesHour;
+      var seatacUl = document.getElementById('seatac');
+      seatacUl.appendChild(liEl);
+      this.cookiesArray.push(this.avgCookiesHour);
+    }
+  },
+  total: function () {
+    this.totalCookies = this.cookiesArray.reduce(function (a,b) {
+      return a + b;
+    }, 0);
+    var liEl = document.createElement('li');
+    liEl.textContent = 'Total ' + this.totalCookies;
+    var seatacUl = document.getElementById('seatac');
+    seatacUl.appendChild(liEl);
+  }
+};
+
+seatac.getAvgCustomersHour();
+seatac.getAvgCookiesHour();
+seatac.render();
+seatac.total();
+console.log('Average Customers per Hour ' + seatac.avgCustomersHour);
+console.log('Average Cookies per Hour ' + seatac.avgCookiesHour);
+console.log(seatac.cookiesArray);
+console.log(seatac.totalCookies);
+//
 // var first = {
 //   storeName: 'First and Pike',
-//   minCustomersDay:
-//   maxCustomersDay:
+//   minCustomers:
+//   maxCustomers:
 //   avgSale:
-//   customersHour:
-//   cookiesHour:
-//   customersDay:
-//   cookiesDay:
-//
-// };
-//
-// var first = {
-//   storeName: 'First and Pike',
-//   minCustomersDay:
-//   maxCustomersDay:
-//   avgSale:
-//   customersHour:
-//   cookiesHour:
-//   customersDay:
-//   cookiesDay:
+//   avgCustomersHour:
+//   avgCookiesHour:
+//   cookiesArray:
+//   totalCookies:
 //
 // };
 //
