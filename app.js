0a8a89c00b75130e7fb00dc56921c639a98f55f7
Kat Cosgrove <kat.cosgrove@gmail.com>
2017-09-25 15:02:44 -0700

    first location fully functional

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+var days = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 var first = {
   storeName: 'First and Pike',
@@ -8,7 +8,8 @@ var first = {
   avgSale: 6.3,
   avgCustomersHour: 0,
   avgCookiesHour: 0,
-  storeNumbers: [this.avgCustomersHour, this.avgCookiesHour],
+  cookiesArray: [],
+  totalCookies: 0,
   getAvgCustomersHour: function (){
     this.avgCustomersHour = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1) + this.minCustomers);
   },
@@ -21,17 +22,30 @@ var first = {
       this.getAvgCookiesHour();
       var liEl = document.createElement('li');
       liEl.textContent = days[i] + ' ' + this.avgCookiesHour;
-      var ggUl = document.getElementById('first');
-      ggUl.appendChild(liEl);
+      var firstUl = document.getElementById('first');
+      firstUl.appendChild(liEl);
+      this.cookiesArray.push(this.avgCookiesHour);
     }
+  },
+  total: function () {
+    this.totalCookies = this.cookiesArray.reduce(function (a,b) {
+      return a + b;
+    }, 0);
+    var liEl = document.createElement('li');
+    liEl.textContent = 'Total ' + this.totalCookies;
+    var firstUl = document.getElementById('first');
+    firstUl.appendChild(liEl);
   }
 };
 
 first.getAvgCustomersHour();
 first.getAvgCookiesHour();
 first.render();
+first.total();
 console.log('Average Customers per Hour ' + first.avgCustomersHour);
 console.log('Average Cookies per Hour ' + first.avgCookiesHour);
+console.log(first.cookiesArray);
+console.log(first.totalCookies);
 
 // var first = {
 //   storeName: 'First and Pike',
