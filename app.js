1796ef2c72be8580598ad95833e1e91a0c3e21ff
Kat Cosgrove <kat.cosgrove@gmail.com>
2017-09-26 16:22:40 -0700

    constructor operates mostly correctly

--- app.js
+++ app.js
@@ -3,9 +3,11 @@
 // Array for business hours
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
-
+// Variable storing array of location objects + variable for table
 var allLocations = [];
+var locationTable = document.getElementById('locationSales');
 
+// Constructor function for stores
 function CookieStore(location, minCustomers, maxCustomers, avgSale) {
   this.location = location;
   this.minCustomers = minCustomers;
@@ -13,50 +15,85 @@ function CookieStore(location, minCustomers, maxCustomers, avgSale) {
   this.avgSale = avgSale;
   this.avgCustomersHour = 0;
   this.avgCookiesHour = 0;
-  this.cookiesArray = [0];
+  this.customersArray = [];
+  this.cookiesArray = [];
   this.totalCookies = 0;
   this.getAvgCustomersHour = function (){
-    this.avgCustomersHour = Math.ceil(Math.random() * (this.maxCustomers - this.minCustomers + 1) + this.minCustomers);
-  };
-  this.getAvgCookiesHour = function () {
     for (var i = 0; i < hours.length; i++){
-      this.avgCookiesHour = Math.ceil(this.avgCustomersHour * this.avgSale);
+      this.avgCustomersHour = Math.ceil(Math.random() * (this.maxCustomers - this.minCustomers + 1) + this.minCustomers);
+      this.customersArray.push(this.avgCustomersHour);
+    }
+  };
+  this.getAvgCookiesHour = function(){
+    for (var i = 0; i < this.customersArray; i++){
+      this.avgCookiesHour = Math.ceil(this.customersArray[i] * this.avgSale);
       this.cookiesArray.push(this.avgCookiesHour);
     }
   };
-  this.getTotalSold = function () {
-    this.totalCookies = this.cookiesArray.reduce(function (a,b) {
-      return a + b;
-    }, 0);
-  };
+  // this.getTotalSold = function(){
+  //   this.totalCookies = this.cookiesArray.reduce(function (a,b) {
+  //     return a + b;
+  //   }, 0);
+  // };
   this.getAvgCustomersHour();
   this.getAvgCookiesHour();
-  this.getTotalSold();
+  // this.getTotalSold();
   allLocations.push(this);
 }
 
+CookieStore.prototype.render = function () {
+  var trEl = document.createElement('tr');
+  var tdEl = document.createElement('td');
+  tdEl.textContent = this.location;
+  trEl.appendChild(tdEl);
+
+  for (var i = 0; i < hours.length; i++){
+    tdEl = document.createElement('td');
+    tdEl.textContent = this.cookiesArray[i];
+    trEl.appendChild(tdEl);
+  }
+  locationTable.appendChild(trEl);
+};
+
 var first = new CookieStore('First and Pike', 23, 65, 6.3);
 var seatac = new CookieStore('SeaTac Airport', 3, 24, 1.2);
-var seaCenter = new CookieStore('Seattle Center',11, 38, 3.7);
-var capHill = new CookieStore('Capitol Hill', 20, 38, 2.3);
-var alki = new CookieStore('Alki', 2, 16, 4.6);
-
-CookieStore.prototype.render = function () {
-  for (var i = 0; i < hours.length; i++) {
-    var liEl = document.createElement('li');
-    liEl.textContent = hours[i] + ' ' + this.avgCookiesHour + ' cookies sold.';
-    var firstUl = document.getElementById('first');
-    firstUl.appendChild(liEl);
-    this.cookiesArray.push(this.avgCookiesHour);
-  }
-};
+// var seaCenter = new CookieStore('Seattle Center',11, 38, 3.7);
+// var capHill = new CookieStore('Capitol Hill', 20, 38, 2.3);
+// var alki = new CookieStore('Alki', 2, 16, 4.6);
 
 console.log(first);
 console.log(seatac);
-console.log(seaCenter);
-console.log(capHill);
-console.log(alki);
+// console.log(seaCenter);
+// console.log(capHill);
+// console.log(alki);
 
+function renderAll() {
+  for (var i in allLocations) {
+    allLocations[i].render();
+  }
+};
+
+function renderHeaders() {
+  var trEl = document.createElement('tr');
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Location';
+  trEl.appendChild(thEl);
+
+  for (var i = 0; i < hours.length; i++) {
+    thEl = document.createElement('th');
+    thEl.textContent = hours[i];
+    trEl.appendChild(thEl);
+  };
+
+  thEl = document.createElement('th');
+  thEl.textContent = 'Total';
+  trEl.appendChild(thEl);
+  locationTable.appendChild(trEl);
+
+}
+
+renderHeaders();
+renderAll();
 
 // // First and Pike Calculation
 // var first = {
